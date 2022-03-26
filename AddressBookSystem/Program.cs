7627ad8f2c8c69ca2f8106ee425814b1b7da89b5
payalbanode0 <payalbanode0@gmail.com>
2022-03-26 15:34:01 +0530

    UC7..

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -4,10 +4,7 @@ using System.Collections.Generic;
 namespace AddressBookSystem
 {
     /* UC7:- Ability to ensure there is no Duplicate Entry of the same Person in a particular Address Book
-             - Duplicate Check is done on Person Name while adding person to Address Book.
-             - Use Collection Methods to Search Person by Name for Duplicate Entry
-             - Override equals method to check for Duplicate
-             - Use Lambda
+           
      */
     class Program
     {
